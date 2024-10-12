683aec83bc717d28fe1b84fc9cacaa66907b478b
ethanamaher <ethanamaher@gmail.com>
2024-10-11 20:07:59 -0500

    added goroutines for calculating next state

--- mnca_sim.go
+++ mnca_sim.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
-    "bufio"
-    "os"
-    "fmt"
-    "strconv"
-    "strings"
-    "math/rand/v2"
+	"math/rand/v2"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -16,106 +18,95 @@ const (
     screenHeight = 384
 )
 
-/*  Rule
- *  @neighborhood   int corresponding to the neighborhood for the rule
- *  @min            float32 min value in interval
- *  @max            float32 max value in interval
- *  @nextState      bool next state if average is in this interval
- */
 type Rule struct {
     neighborhood int
-    min float32
-    max float32
+    min, max float32
     nextState bool
 }
 
 // Returns where a value is inside a Rule's interval
-func (i *Rule) Contains(val float32) bool {
-    return val >= i.min && val <= i.max
+func (r *Rule) Contains(val float32) bool {
+    return val >= r.min && val <= r.max
 }
 
-// x y coordinate
 type Coordinate struct {
-    x int
-    y int
+    x, y int
 }
 
-/*  Neighborhood
- *  @validNeighbors int number of valid neighbors (len of neighbors[])
- *  @neighbors      []Coordinate of relative coordinates for neighboring cells
- */
 type Neighborhood struct {
     validNeighbors int
     neighbors []Coordinate
 }
 
-/*  EvolutionRules
- *  @numNeighborhoods   int number of neighborhoods
- *  @neighborhoods      []Neighborhood
- *  @rulesList          []Rule
- */
 type EvolutionRules struct {
     numNeighborhoods int
     neighborhoods []Neighborhood
     rulesList []Rule
 }
 
-/*  World
- *  @grid   []bool current state of World
- *  @width  int columns
- *  @height int rows
- *  @rules  EvolutionRules for World
- */
 type World struct {
-    grid []bool
-    width int
-    height int
+    grid, nextGrid []bool
+    width, height int
     rules EvolutionRules
 }
 
 func InitializeWorld(width, height int) *World {
     w := &World {
         grid: make([]bool, width*height),
+        nextGrid: make([]bool, width*height),
         width: width,
         height: height,
         rules: readNeighborhoods(),
     }
 
-    for y := 0; y < w.height; y++ {
-        for x := 0; x < w.width; x++ {
-            w.grid[y*w.width+x] = rand.IntN(100) <= 60
-        }
+    for i := range w.grid {
+        w.grid[i] = rand.IntN(100) < 60
     }
+
     return w
 }
 
 func (w *World) Update() {
-    width := w.width
-    height := w.height
-    next := make([]bool, width*height)
+    var wg sync.WaitGroup
+    rowsPerRoutine := w.height/8
 
-    for y := 0; y < height; y++ {
-        for x := 0; x < width; x++ {
-            sumAvgs := make([]float32, w.rules.numNeighborhoods)
+    for startY := 0; startY < w.height; startY += rowsPerRoutine {
+        wg.Add(1)
+        endY := startY + rowsPerRoutine
+        if endY > w.height {
+            endY = w.height
+        }
 
-            // calculate % of alive neighbors
-            for i, _ := range sumAvgs {
-                nCount := neighborCount(w.grid, w.width, w.height, x, y, &w.rules.neighborhoods[i])
-                sumAvgs[i] = float32(nCount) / float32(w.rules.neighborhoods[i].validNeighbors)
-            }
+        go func(start, end int) {
+            defer wg.Done()
+            for y := start; y < end; y++ {
+                for x := 0; x < w.width; x++ {
+                    sumAvgs := make([]float32, len(w.rules.neighborhoods))
 
-            // calculate next state based on EvolutionRules
-            next[y*width+x] = w.grid[y*width+x]
-            for _, interval := range w.rules.rulesList {
-                if interval.Contains(sumAvgs[interval.neighborhood]) {
-                    next[y*width+x] = interval.nextState
+                    // calculate % of alive neighbors
+                    for i, neighborhood := range w.rules.neighborhoods {
+                        nCount := neighborCount(w.grid, w.width, w.height, x, y, &neighborhood)
+                        sumAvgs[i] = float32(nCount) / float32(neighborhood.validNeighbors)
+                    }
+
+                    // calculate next state based on EvolutionRules
+                    nextState := w.grid[y*w.width+x]
+                    for _, rule := range w.rules.rulesList {
+                        if rule.Contains(sumAvgs[rule.neighborhood]) {
+                        nextState = rule.nextState
+                        }
+                    }
+                    w.nextGrid[y*w.width+x] = nextState
                 }
             }
-        }
+        }(startY, endY)
+
+
     }
 
-    // update grid with next state
-    w.grid = next
+    wg.Wait()
+    // swap grids
+    w.grid, w.nextGrid = w.nextGrid, w.grid
 }
 
 func neighborCount(a []bool, width, height, x, y int, n *Neighborhood) int {
@@ -139,26 +130,20 @@ func neighborCount(a []bool, width, height, x, y int, n *Neighborhood) int {
 func (w *World) Draw(pix []byte) {
     for i, v := range w.grid {
         if v {
-            pix[4*i] = 0xff
-            pix[4*i+1] = 0xff
-            pix[4*i+2] = 0xff
-            pix[4*i+3] = 0xff
+            pix[4*i], pix[4*i+1], pix[4*i+2], pix[4*i+3] = 0xff, 0xff, 0xff, 0xff
         } else {
-            pix[4*i] = 0
-            pix[4*i+1] = 0
-            pix[4*i+2] = 0
-            pix[4*i+3] = 0
+            pix[4*i], pix[4*i+1], pix[4*i+2], pix[4*i+3] = 0, 0, 0, 0
         }
     }
 }
 
 type Game struct {
-    grid *World
+    world *World
     pixels []byte
 }
 
 func (g *Game) Update() error {
-    g.grid.Update()
+    g.world.Update()
 	return nil
 }
 
@@ -166,7 +151,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
     if g.pixels == nil {
         g.pixels = make([]byte, screenWidth*screenHeight*4)
     }
-    g.grid.Draw(g.pixels)
+    g.world.Draw(g.pixels)
     screen.WritePixels(g.pixels)
 }
 
@@ -174,7 +159,6 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
-// this is probably not great
 func readNeighborhoods() (EvolutionRules) {
     file, err := os.Open("rules.txt")
     if err != nil {
@@ -185,107 +169,64 @@ func readNeighborhoods() (EvolutionRules) {
     scanner := bufio.NewScanner(file)
 
     scanner.Scan()
-    n, err := strconv.Atoi(scanner.Text())
+    numNeighborhoods, err := strconv.Atoi(scanner.Text())
     if err != nil {
         log.Fatal(err)
     }
-    defer file.Close()
 
-    r := EvolutionRules{
-        numNeighborhoods: n,
-        neighborhoods: make([]Neighborhood, n),
-        rulesList: nil,
-    }
+    neighborhoods := make([]Neighborhood, numNeighborhoods)
 
-    for i := 0; i < n; i++ {
+    for i := 0; i < numNeighborhoods; i++ {
         scanner.Scan()
-        numNeighbors, err := strconv.Atoi(scanner.Text())
-        if err != nil {
-            log.Fatal(err)
-        }
+        numNeighbors, _ := strconv.Atoi(scanner.Text())
 
-        defer file.Close()
-
-        currentNeighborhood := Neighborhood{
-            validNeighbors: numNeighbors,
-            neighbors: make([]Coordinate, numNeighbors),
-        }
-
+        neighbors := make([]Coordinate, numNeighbors)
         for j := 0; j < numNeighbors; j++ {
             scanner.Scan()
-            line := scanner.Text()
-            fields := strings.Fields(line)
+            fields := strings.Fields(scanner.Text())
 
-            if len(fields) != 2 {
-                fmt.Println("Invalid line format: ", line)
-                continue
-            }
+            x, _ := strconv.Atoi(fields[0])
+            y, _ := strconv.Atoi(fields[1])
 
-            int1, err1 := strconv.Atoi(fields[0])
-            int2, err2 := strconv.Atoi(fields[1])
-
-            if err1 != nil || err2 != nil {
-                fmt.Println("Error converting fields: ", fields)
-                continue
-            }
-
-            coordinate := Coordinate{
-                x: int1,
-                y: int2,
-            }
-
-            currentNeighborhood.neighbors[j] = coordinate
+            neighbors[j] = Coordinate{x, y}
         }
-        r.neighborhoods[i] = currentNeighborhood
+        neighborhoods[i] = Neighborhood{validNeighbors: numNeighbors, neighbors: neighbors}
     }
 
-    fmt.Printf("Successfully loaded %d neighborhoods\n", len(r.neighborhoods))
+    fmt.Printf("Successfully loaded %d neighborhoods\n", len(neighborhoods))
+
     scanner.Scan()
-    rCount, err := strconv.Atoi(scanner.Text())
-    if err != nil {
-        fmt.Println("Could not read rule count")
-    }
-    defer file.Close()
+    ruleCount, _ := strconv.Atoi(scanner.Text())
 
-    r.rulesList = make([]Rule, rCount)
+    rulesList := make([]Rule, ruleCount)
 
-    for i := 0; i < rCount; i++ {
+    for i := 0; i < ruleCount; i++ {
         scanner.Scan()
-        line := scanner.Text()
-        fields := strings.Fields(line)
+        fields := strings.Fields(scanner.Text())
 
-        if len(fields) != 4 {
-            fmt.Println("Error reading line: ", line)
-            continue
-        }
+        neighborhoodNum, _ := strconv.Atoi(fields[0])
+        intervalMin, _ := strconv.ParseFloat(fields[1], 32)
+        intervalMax, _ := strconv.ParseFloat(fields[2], 32)
+        nextState, _ := strconv.ParseBool(fields[3])
 
-        neighborhoodNum, err1 := strconv.Atoi(fields[0])
-        intervalMin, err2 := strconv.ParseFloat(fields[1], 32)
-        intervalMax, err3 := strconv.ParseFloat(fields[2], 32)
-        nextState, err4 := strconv.ParseBool(fields[3])
-
-        if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
-            fmt.Println("error reading fields", fields)
-            continue
-        }
-
-        interval := Rule{
+        rulesList[i] = Rule{
             neighborhood: neighborhoodNum,
             min: float32(intervalMin),
             max: float32(intervalMax),
             nextState: nextState,
         }
-
-        r.rulesList[i] = interval
     }
 
-    fmt.Printf("Successfully loaded %d neighborhood rules\n", len(r.rulesList))
-    return r
+    fmt.Printf("Successfully loaded %d neighborhood rules\n", len(rulesList))
+    return EvolutionRules{
+        neighborhoods: neighborhoods,
+        rulesList: rulesList,
+    }
 }
 
 func main() {
     g := &Game {
-        grid: InitializeWorld(screenWidth, screenHeight),
+        world: InitializeWorld(screenWidth, screenHeight),
     }
 
 	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
